fc1a9dd28f966e2ef802a97acbc1301349b98ea9
Falko Gebel <63905869+FalkoGebel@users.noreply.github.com>
2024-03-24 17:41:54 +0100

    worked on UI

--- Sql2ExcelExporterUI/MainWindow.xaml.cs
+++ Sql2ExcelExporterUI/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using ExporterLogicLibrary;
+using ExporterLogicLibrary.Models;
+using Sql2ExcelExporterUI.Models;
 using System.Windows;
 
 namespace Sql2ExcelExporterUI
@@ -8,6 +10,8 @@ namespace Sql2ExcelExporterUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<ColumnsListViewModel> _columns = [];
+
         public MainWindow()
         {
             InitializeComponent();
@@ -23,9 +27,9 @@ namespace Sql2ExcelExporterUI
             try
             {
                 List<string> databases = SqlLogic.GetDatabasesFromServer(ServerTextBox.Text);
-                DatabaseChoiceWindow dbcw = new(databases);
+                ChoiceWindow dbcw = new(Properties.Resources.DBCW_TITLE, databases);
                 dbcw.ShowDialog();
-                DatabaseTextBox.Text = dbcw.GetDatabase();
+                DatabaseTextBox.Text = dbcw.GetChoice();
             }
             catch (Exception e)
             {
@@ -38,5 +42,68 @@ namespace Sql2ExcelExporterUI
             MessageBox.Show(msg, Properties.Resources.ERROR_TITLE, MessageBoxButton.OK, MessageBoxImage.Error);
         }
 
+        private void UpdateColumnsListView(bool fromDatabase)
+        {
+            if (fromDatabase)
+            {
+                _columns = [];
+
+                if (ServerTextBox.Text == string.Empty || DatabaseTextBox.Text == string.Empty)
+                    return;
+
+                foreach (ColumnModel col in SqlLogic.GetColumnsForTable(ServerTextBox.Text, DatabaseTextBox.Text, TableTextBox.Text).OrderBy(cm => cm.Name))
+                    _columns.Add(new ColumnsListViewModel() { Name = col.Name, Selected = true, Type = col.Type });
+            }
+
+            ColumnsListView.ItemsSource = null;
+            ColumnsListView.ItemsSource = _columns;
+        }
+
+        private void TableAssistButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenTableChoiceWindow();
+        }
+
+        private void OpenTableChoiceWindow()
+        {
+            try
+            {
+                List<string> tables = SqlLogic.GetTablesForDatabase(ServerTextBox.Text, DatabaseTextBox.Text);
+                ChoiceWindow dbcw = new(Properties.Resources.TCW_TITLE, tables);
+                dbcw.ShowDialog();
+                TableTextBox.Text = dbcw.GetChoice();
+                UpdateColumnsListView(true);
+            }
+            catch (Exception e)
+            {
+                ShowError(e.Message);
+            }
+        }
+
+        private void SelectAllColumnsButton_Click(object sender, RoutedEventArgs e)
+        {
+            SelectAllListViewColumns();
+        }
+
+        private void SelectAllListViewColumns()
+        {
+            foreach (var column in _columns)
+                column.Selected = true;
+
+            UpdateColumnsListView(false);
+        }
+
+        private void SelectNoneColumnsButton_Click(object sender, RoutedEventArgs e)
+        {
+            SelectNoneListViewColumns();
+        }
+
+        private void SelectNoneListViewColumns()
+        {
+            foreach (var column in _columns)
+                column.Selected = false;
+
+            UpdateColumnsListView(false);
+        }
     }
 }
---
+++ Sql2ExcelExporterUI/Models/ColumnsListViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Sql2ExcelExporterUI.Models
+{
+    public class ColumnsListViewModel
+    {
+        public bool Selected { get; set; }
+        public required string Name { get; set; }
+        public required string Type { get; set; }
+    }
+}
